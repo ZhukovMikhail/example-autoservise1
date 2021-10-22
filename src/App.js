85143207b249cbeeccba30b1e0893d8267ffe2f7
ZhukovMikhail <79285451+ZhukovMikhail@users.noreply.github.com>
2021-10-23 01:56:14 +0300

    navBar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavigationBar from './Components/NavigationBar';
+import Hero from './Components/Hero/Hero';
+
+function App() {
+  return (
+    <>
+      <NavigationBar />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+// import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import styles from './Hero.module.css';
+
+export default function Hero() {
+  return <div className={styles.heroBg}></div>;
+}
